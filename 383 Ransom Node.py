3a42a1d71310f5947f91f4874445af4fd285436b
Sue <spuahss@gmail.com>
2024-11-22 00:13:26 +0800

    383 ver1

---
+++ 383 Ransom Node.py
@@ -0,0 +1,18 @@
+# Easy
+# 383 Ransom Node
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+# Each letter in magazine can only be used once in ransomNote.
+
+# Base Code
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+
+# Solution
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        
