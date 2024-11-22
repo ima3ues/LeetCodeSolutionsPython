ee8a9b06144e88ba025bf91756f916eae65a059c
Sue <spuahss@gmail.com>
2024-11-23 00:12:21 +0800

    977 ver4

--- 977 Array Squares of a Sorted Array.py
+++ 977 Array Squares of a Sorted Array.py
@@ -1,4 +1,4 @@
-# Easy **need to revise again as i couldn't get the ans quickly**
+# Easy 
 # 977 Squares of a Sorted Array (Array)
 # Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 
