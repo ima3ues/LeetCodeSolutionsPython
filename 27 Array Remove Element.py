248705aafb22495ffad21b51325eaab155df7958
Sue <spuahss@gmail.com>
2024-11-26 22:52:23 +0800

    27 ver3

--- 27 Array Remove Element.py
+++ 27 Array Remove Element.py
@@ -14,9 +14,22 @@ class Solution(object):
         :rtype: int
         """
 
-# Solution
+# Solution (wrong version which seems correct)
 class Solution(object):
     def removeElement(self, nums, val):
         for each_num in nums:
             if each_num == val:
                 nums.remove(each_num)
+# Explanation:
+# when we use for...in..., python will automatically generate index
+# when we use .remove, python will automatically move the value to the most left
+# so for example, when the input is nums = [0,1,2,2,3,0,4,2] and val = 2
+# at the 3rd iteration nums[2] = 2  → True, nums = [0, 1, 2, 3, 0, 4, 2] 
+# the problem occurs at the 4th iteration where nums[3] = 3 -> False, nums = [0, 1, 2, 3, 0, 4, 2]
+# continuously loop until the 7th iteration where nums[6] - 2 -> True, nums = [0, 1, 3, 0, 4, 2], this happens as nums.remove(each_num) removes the first occurrence of the value in the list, not the value at the current index
+# therefore the final result was nums = [0, 1, 3, 0, 4, 2]
+
+# Solution
+class Solution(object):
+    def removeElement(self, nums, val):
+        
