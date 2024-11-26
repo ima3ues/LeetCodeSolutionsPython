4694af241aae954597574a2046816da345193b7b
Sue <spuahss@gmail.com>
2024-11-27 01:40:01 +0800

    941 ver1

---
+++ 941 Array Valid Mountain Array.py
@@ -0,0 +1,20 @@
+# Easy
+# 941 Valid Mountain Array (Array)\
+# Given an array of integers arr, return true if and only if it is a valid mountain array.
+# Recall that arr is a mountain array if and only if:
+# arr.length >= 3
+# There exists some i with 0 < i < arr.length - 1 such that:
+# arr[0] < arr[1] < ... < arr[i - 1] < arr[i] 
+# arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+# Base Code
+class Solution(object):
+    def validMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+
+# Solution
+class Solution(object):
+    def validMountainArray(self, arr):
