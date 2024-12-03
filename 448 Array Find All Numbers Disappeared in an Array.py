a3e782bd44e315dd17148dffee088a63e86d5e2b
Sue <spuahss@gmail.com>
2024-12-04 05:45:39 +0800

    448 ver2

--- 448 Array Find All Numbers Disappeared in an Array.py
+++ 448 Array Find All Numbers Disappeared in an Array.py
@@ -13,3 +13,16 @@ class Solution(object):
 # Solution
 class Solution(object):
     def findDisappearedNumbers(self, nums):
+        duplicate_nums = []
+        for each_num in nums:
+            if each_num not in duplicate_nums:
+                duplicate_nums.append(each_num)
+        duplicate_nums.sort()
+        i = 0
+        counter = 1
+        return_list = []
+        for i in range(len(nums) - 1):
+            if duplicate_nums[i] != counter:
+                return_list.append(i)
+                counter += 1
+        return return_list
