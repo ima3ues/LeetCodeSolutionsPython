bc8d8236a5e748653ea0c0424ee5379a5f140982
Sue <spuahss@gmail.com>
2024-12-04 17:35:34 +0800

    724 ver2

--- 724 Find Pivot Index.py
+++ 724 Find Pivot Index.py
@@ -1,9 +1,9 @@
 # Easy
 # 724 Find Pivot Index
-# Given an array of integers nums, calculate the pivot index of this array.
-# The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+# Given an array of integers nums, calculate the pivot_index index of this array.
+# The pivot_index index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
 # If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
-# Return the leftmost pivot index. If no such index exists, return -1.
+# Return the leftmost pivot_index index. If no such index exists, return -1.
 
 # Base Code
 class Solution(object):
@@ -13,23 +13,22 @@ class Solution(object):
         :rtype: int
         """
 
-# Solution
+# Solution (which is correct but exceed the time limit)
 class Solution(object):
     def pivotIndex(self, nums):
-        pivot = 0
-        for pivot in range(len(nums) - 1):
+        for pivot_index in range(len(nums)):
             right_accumulator = 0
             left_accumulator = 0
             i = 0
-            while i < pivot:
+            while i < pivot_index:
                 right_accumulator += nums[i]
                 i += 1
             j = len(nums) - 1
-            while j > pivot:
+            while j > pivot_index:
                 left_accumulator += nums[j]
                 j -= 1
             if right_accumulator == left_accumulator:
-                return pivot
+                return pivot_index
             else:
-                pivot += 1
+                pivot_index += 1
         return -1
