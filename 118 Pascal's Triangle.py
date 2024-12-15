1b79a317689a635cff81fd0ab599d79923dfc352
Sue <spuahss@gmail.com>
2024-12-16 01:19:04 +0800

    118 ver1

---
+++ 118 Pascal's Triangle.py
@@ -0,0 +1,17 @@
+# Easy
+# 118 Pascal's Triangle
+# Given an integer numRows, return the first numRows of Pascal's triangle.
+
+# Base Code
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        
+# Solution
+class Solution(object):
+    def generate(self, numRows):
+        
+        
