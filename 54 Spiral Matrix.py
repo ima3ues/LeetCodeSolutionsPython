b2e8c10419f02ebcc3bf98204aa26286a9e74364
Sue <spuahss@gmail.com>
2024-12-16 01:15:49 +0800

    54 ver3

--- 54 Spiral Matrix.py
+++ 54 Spiral Matrix.py
@@ -13,8 +13,50 @@ class Solution(object):
 # Solution
 class Solution(object):
     def spiralOrder(self, matrix):
-        row = len(matrix)
-        column = len(matrix[0])
+        rows = len(matrix)
+        columns = len(matrix[0])
         i = 0
         j = 0
-        
+        return_list = []
+        direction = 1
+
+        current_col = columns - 1
+        current_row = rows - 1
+        current_col2 = 0
+        current_row2 = 1
+        while len(return_list) < rows * columns:
+            return_list.append(matrix[i][j])
+
+            if direction == 1:
+                if j == current_col:
+                    current_col -= 1
+                    i += 1
+                    direction = 2
+                else:
+                    j += 1
+
+            elif direction == 2:
+                if i == current_row:
+                    current_row -= 1
+                    j -= 1
+                    direction = 3
+                else:
+                    i += 1
+
+            elif direction == 3:
+                if j == current_col2:
+                    current_col2 += 1
+                    i -= 1
+                    direction = 4
+                else:
+                    j -= 1
+
+            elif direction == 4:
+                if i == current_row2:
+                    current_row2 += 1
+                    j += 1
+                    direction = 1
+                else:
+                    i -= 1
+
+        return return_list
