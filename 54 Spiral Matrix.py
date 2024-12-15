34a2dc0a4fe7d0944f5d85b7820e4e68b8fee4db
Sue <spuahss@gmail.com>
2024-12-15 14:13:55 +0800

    54 ver1

---
+++ 54 Spiral Matrix.py
@@ -0,0 +1,15 @@
+# Medium
+# 54 Spiral Matrix
+# Given an m x n matrix, return all elements of the matrix in spiral order.
+
+# Base Code
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+
+# Solution
+class Solution(object):
+    def spiralOrder(self, matrix):
