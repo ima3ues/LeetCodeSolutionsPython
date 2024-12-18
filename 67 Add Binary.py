3d898088f4d6abc214a3638ae680e57910dd8b4f
Sue <spuahss@gmail.com>
2024-12-19 00:39:40 +0800

    67 ver2

--- 67 Add Binary.py
+++ 67 Add Binary.py
@@ -14,83 +14,82 @@ class Solution(object):
 # Solution
 class Solution(object):
     def addBinary(self, a, b):
-        length_a = len(a)
-        length_b = len(b)
-        compare = max(length_a, length_b)
+        leak = None
+        index_a = len(a) - 1
+        index_b = len(b) - 1
+        return_list = []
 
+        while leak != "done":
+            
+            if leak == None:
+                if index_a < 0 and index_b < 0:
+                    leak = "done"
 
-        i = 0
-        prev_num = None
-
-
-
-        while i < compare:
-
-
-
-            if prev_num == None:
-                if index_a < 0: 
+                elif index_a < 0: 
                     now = b[index_b]
-                    prev_num = None
+                    leak = None
 
                 elif index_b < 0:
                     now = a[index_a] 
-                    prev_num = None
+                    leak = None
 
                 else:
                     num = a[index_a] + b[index_b]
                     if num == 0 or num == 1:
                         now = num
-                        prev_num = None
+                        leak = None
                     elif num == 2:
                         now = 0
-                        prev_num = 1
+                        leak = 1
                     elif num == 3:
                         now = 1
-                        prev_num = 1
+                        leak = 1
                 
-                returnl.append(now)
-
-
             else:
                 if index_a < 0 and index_b < 0:
-                    now = prev_num
+                    now = leak
 
                 elif index_a < 0: 
-                    num = b[index_b] + prev_num
+                    num = b[index_b] + leak
                     if num == 0 or num == 1:
                         now = num
-                        prev_num = None
+                        leak = None
                     elif num == 2:
                         now = 0
-                        prev_num = 1
+                        leak = 1
                     elif num == 3:
                         now = 1
-                        prev_num = 1
+                        leak = 1
 
                 elif index_b < 0:
-                    num = a[index_a] + prev_num 
-                    prev_num = None
+                    num = a[index_a] + leak 
+                    leak = None
                     if num == 0 or num == 1:
                         now = num
-                        prev_num = None
+                        leak = None
                     elif num == 2:
                         now = 0
-                        prev_num = 1
+                        leak = 1
                     elif num == 3:
                         now = 1
-                        prev_num = 1
+                        leak = 1
 
                 else:
-                    num = a[index_a] + b[index_b] + prev_num
+                    num = a[index_a] + b[index_b] + leak
                     if num == 0 or num == 1:
                         now = num
-                        prev_num = None
+                        leak = None
                     elif num == 2:
                         now = 0
-                        prev_num = 1
+                        leak = 1
                     elif num == 3:
                         now = 1
-                        prev_num = 1
+                        leak = 1
 
-            i += 1
+            return_list.append(now)
+            index_a -= 1
+            index_b -= 1
+        
+        return_list.reverse()
+        last = "".join(return_list)
+        return(last)
