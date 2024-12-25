564e98f923d8045bd88cb2a2688b88729e348121
Sue <spuahss@gmail.com>
2024-12-25 19:42:13 +0800

    14 ver2

--- 14 Longest Common Prefix.py
+++ 14 Longest Common Prefix.py
@@ -16,12 +16,13 @@ class Solution(object):
     # just a reminder, the question asked for prefix
     def longestCommonPrefix(self, strs):
         i = 0
-        accumulator = ""
-        for each_alpha in strs[0]:
-            if strs[0][i] == each_alpha:
-                i += 1
-                accumulator = accumulator + each_alpha
-            else:
-                break
-
-        return accumulator
+        while i < len(strs):
+            j = 0
+            while j < len(accumulator):
+                if accumulator[j] == strs[i][j]:
+                    j += 1
+                    accumulator_curr = accumulator_curr + strs[i-1][j]
+                else:
+                    break
+            accumulator = accumulator_curr
+            i += 1
